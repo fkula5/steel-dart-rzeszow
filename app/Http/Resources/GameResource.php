b61f64e44b4b21d4edfa05a8836aad5b7d3bb63a
Filip Kula <f3kula@gmail.com>
2023-09-17 21:49:20 +0200

    fix: applied CR advices

--- app/Http/Resources/GameResource.php
+++ app/Http/Resources/GameResource.php
@@ -16,24 +16,25 @@ class GameResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
+        $game = Game::find($this->id);
         return [
             'id' => $this->id,
             'player_one' => new PlayerResource($this->whenLoaded('playerOne')),
             'player_two' => new PlayerResource($this->whenLoaded('playerTwo')),
             'player_one_score' => $this->player_one_score,
             'player_two_score' => $this->player_two_score,
-            'player_one_avg' => $this->player_one_avg,
-            'player_two_avg' => $this->player_two_avg,
+            'player_one_avg' => (float)$this->player_one_avg,
+            'player_two_avg' => (float)$this->player_two_avg,
             'player_one_max_amount' => $this->player_one_max_amount,
             'player_two_max_amount' => $this->player_two_max_amount,
-            'player_one_high_outs' => new HighOutCollection(Game::find($this->id)->highOuts()->where('player_id', $this->player_one)->get()),
-            'player_two_high_outs' => new HighOutCollection(Game::find($this->id)->highOuts()->where('player_id', $this->player_two)->get()),
-            'player_one_fast_outs' => new FastOutCollection(Game::find($this->id)->fastOuts()->where('player_id', $this->player_one)->get()),
-            'player_two_fast_outs' => new FastOutCollection(Game::find($this->id)->fastOuts()->where('player_id', $this->player_two)->get()),
-            'league' => League::findOrFail($this->league_id),
+            'player_one_high_outs' => new HighOutCollection($game->getPlayerHighOuts($this->player_one)),
+            'player_two_high_outs' => new HighOutCollection($game->getPlayerHighOuts($this->player_two)),
+            'player_one_fast_outs' => new FastOutCollection($game->getPlayerFastOuts($this->player_one)),
+            'player_two_fast_outs' => new FastOutCollection($game->getPlayerFastOuts($this->player_two)),
+            'league' => new LeagueResource($this->whenLoaded('league')),
             'winner' => $this->winner,
-            'created_at' => $this->created_at,
-            'updated_at' => $this->updated_at
+            'created_at' => $this->created_at->format('d.m.Y'),
+            'updated_at' => $this->updated_at->format('d.m.Y')
         ];
     }
 }
--- app/Models/Game.php
+++ app/Models/Game.php
@@ -6,12 +6,20 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Collection;
 
 class Game extends Model
 {
     use HasFactory;
 
     protected $fillable = ['player_one', 'player_two', 'player_one_score', 'player_two_score', 'player_one_avg', 'player_two_avg', 'player_one_max_amount', 'player_two_max_amount', 'league_id', 'winner'];
+
+    public function getPlayerHighOuts($playerId): Collection {
+        return $this->highOuts()->where('player_id', $playerId)->get();
+    }
+    public function getPlayerFastOuts($playerId): Collection {
+        return $this->fastOuts()->where('player_id', $playerId)->get();
+    }
 
     public function highOuts(): HasMany
     {
